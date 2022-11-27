d349183b4fd633ea2e699de1c3a910a2d2e1b2c4
Pavl01207 <112820959+Pavl01207@users.noreply.github.com>
2022-11-27 13:27:13 +0200

    Add files via upload

---
+++ javascript/script.js
@@ -0,0 +1,48 @@
+"use strict"
+
+console.log(`Я вивчаю JavaScript`);
+//
+let colorEyeUser;
+colorEyeUser = `Green`;
+console.log(colorEyeUser);
+//
+let user, userName;
+userName = "Vasya";
+user = userName;
+console.log(user);
+//
+const myName = `Pavlo`;
+const myAge = `29`;
+const myInfo = `Name:${myName},age:${myAge}`;
+console.log(myInfo);
+//
+const mySecondName = `Palahniuk`;
+console.log(typeof mySecondName);
+//
+const myWeight = 84;
+console.log(typeof myWeight);
+//
+let myJob;
+console.log(typeof myJob);
+//
+let userMoney, userCash;
+userMoney = `100`;
+userCash = `10`;
+let userBuy = userCash/userMoney;
+console.log(userBuy);
+console.log(typeof userBuy);
+//
+const someVar = 100<10;
+if (someVar){
+	console.log(`No way`)
+}
+else{
+	console.log(`Problem`)
+}
+//
+let myWifeAge = 25;
+console.log(myWifeAge);
+console.log(typeof myWifeAge);
+myWifeAge = String(myWifeAge);
+console.log(myWifeAge);
+console.log(typeof myWifeAge);
